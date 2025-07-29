de50620c70c0abb98608a666069590dce4c20689
Girish312 <fs20if057@gmail.com>
2025-07-29 21:07:17 +0530

    live session

--- Day2/Live Session/main.py
+++ Day2/Live Session/main.py
@@ -10,3 +10,60 @@ e = None
 print(type(a), type(b), type(c), type(d), type(e))
 """
 
+"""
+#arithmetic
+a=5
+b=10
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+
+#asignment
+a=10
+
+print(a)
+a+=5
+print(a)
+a-=5
+print(a)
+a*=2
+print(a)
+a/=5
+print(a)
+a**=4
+print(a)
+a//=5
+print(a)
+a%=2
+print(a)
+
+#Comparison
+a=10
+b=5
+
+print(a==b)
+print(a!=b)
+print(a<b)
+print(a>b)
+print(a<=b)
+print(a>=b)
+
+#logical
+a=10
+b=15
+if (a>5 and a<20):
+    print(True)
+else:
+    print(False)
+
+if not(b>15 or a<5):
+    print(True)
+else:
+    print(False)
+"""
