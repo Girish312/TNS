87750754e00bf396e5b8c9f0699ac3d5ec7a62f7
Girish312 <fs20if057@gmail.com>
2025-07-30 09:51:57 +0530

    Day3

---
+++ Day3/main.py
@@ -0,0 +1,14 @@
+"""
+for i in range(0,6):
+    print("Hello World")
+
+i=0
+while (i<5):
+    print("Hello World")
+    i+=2
+"""
+for i in range(5):
+    print("and")
+    for j in range(6):
+        for x in range(2):
+            print("Hello World")
