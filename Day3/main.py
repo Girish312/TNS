c5c75397969973c7cd26f1bb3063376358dcaea7
Girish312 <fs20if057@gmail.com>
2025-07-30 10:10:37 +0530

    Day3

--- Day3/main.py
+++ Day3/main.py
@@ -6,9 +6,26 @@ i=0
 while (i<5):
     print("Hello World")
     i+=2
-"""
+
 for i in range(5):
-    print("and")
+    print("Runs 5 times")
     for j in range(6):
+        print("Runs 6 times")
         for x in range(2):
-            print("Hello World")
+            print("Runs 2 times")
+"""
+
+"""
+def greet():
+    print("Hello Girish")
+greet()
+"""
+a = 10
+def ageChecking ():
+    if (a<18):
+        for i in range(5):
+            print("Under Age- Warning")
+    else:
+        print("Approved")
+
+ageChecking()
